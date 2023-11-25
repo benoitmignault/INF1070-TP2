8b81cf0789a065d4e2fda735e23a052bb5d9f6cf
Benoit Mignault <mignault.benoit@courrier.uqam.ca>
2023-11-24 22:27:06 -0500

    Nettoyage du script mystere/mystere.sh

--- mystere/mystere.sh
+++ mystere/mystere.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 if [ $# -ne 1 ]; then
     echo "Fournir un nom en parametre"
     exit 1
@@ -8,5 +9,7 @@ if [ -d "$1" ]; then
     exit 0
 else
     "$0" `dirname "$1"`
+    echo "Après la récursivité $1"
+
     mkdir "$1"
 fi
